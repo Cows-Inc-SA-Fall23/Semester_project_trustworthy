12a5ee395f1d4f6ee6e888057e5c141d364d08c4
GitButler <gitbutler@gitbutler.com>
2024-02-27 08:53:30 +0100

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - Added allocation view description
     (refs/gitbutler/Added-allocation-view-description-)
       branch head: 88893060e850a401f10f5fb8efc0e666e5d073af
       - esp32/target/debug/deps/esp32_embedded-611710be268b0ee5.d
       - esp32/target/debug/.fingerprint/esp32_embedded-611710be268b0ee5/test-bin-esp32_embedded
       - esp32/target/debug/.fingerprint/esp32_embedded-611710be268b0ee5/dep-test-bin-esp32_embedded
       - README.md
       - esp32/target/debug/.fingerprint/esp32_embedded-c7be5c301cb57ea5/bin-esp32_embedded.json
       - esp32/target/debug/incremental/esp32_embedded-2ujl4v9vaammr/s-gtrzuqduaf-14at46w-dzlb1mzfbe8luidtxcmgyz7du/query-cache.bin
       - esp32/target/debug/.fingerprint/esp32_embedded-c7be5c301cb57ea5/invoked.timestamp
       - esp32/target/.rustc_info.json
       - esp32/target/debug/.fingerprint/esp32_embedded-c7be5c301cb57ea5/bin-esp32_embedded
       - Architecture/Allocation_DoorLock.drawio
       - esp32/target/debug/deps/libesp32_embedded-c7be5c301cb57ea5.rmeta
       - esp32/Cargo.lock
       - esp32/target/debug/incremental/esp32_embedded-2ujl4v9vaammr/s-gtrzuqduaf-14at46w-dzlb1mzfbe8luidtxcmgyz7du/dep-graph.bin
       - esp32/target/debug/.cargo-lock
       - esp32/target/debug/deps/libesp32_embedded-611710be268b0ee5.rmeta
       - Architecture/Allocation View/Allocation_DoorLock.drawio.svg
       - Architecture/Allocation View/Description.md
       - esp32/target/CACHEDIR.TAG
       - esp32/target/debug/incremental/esp32_embedded-2pqif8263h58n/s-gtrzuqduve-12exeqo-7v43zz1p5mqxya01lgp1mhmws/dep-graph.bin
       - esp32/target/debug/.fingerprint/esp32_embedded-c7be5c301cb57ea5/dep-bin-esp32_embedded
       - esp32/target/debug/incremental/esp32_embedded-2pqif8263h58n/s-gtrzuqduve-12exeqo-7v43zz1p5mqxya01lgp1mhmws/query-cache.bin
       - esp32/target/debug/incremental/esp32_embedded-2pqif8263h58n/s-gtrzuqduve-12exeqo-7v43zz1p5mqxya01lgp1mhmws/work-products.bin
       - esp32/target/debug/deps/esp32_embedded-c7be5c301cb57ea5.d
       - esp32/target/debug/incremental/esp32_embedded-2pqif8263h58n/s-gtrzuqduve-12exeqo.lock
       - esp32/target/debug/incremental/esp32_embedded-2ujl4v9vaammr/s-gtrzuqduaf-14at46w.lock
       - esp32/target/debug/.fingerprint/esp32_embedded-611710be268b0ee5/invoked.timestamp
       - esp32/target/debug/incremental/esp32_embedded-2ujl4v9vaammr/s-gtrzuqduaf-14at46w-dzlb1mzfbe8luidtxcmgyz7du/work-products.bin
       - esp32/target/debug/.fingerprint/esp32_embedded-611710be268b0ee5/test-bin-esp32_embedded.json
    
    Your previous branch was: refs/heads/master
    
    The sha for that commit was: 1a179d963f656e4ee3596862f64af0666247cf7d
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- esp32/target/debug/deps/esp32_embedded-c7be5c301cb57ea5.d
+++
@@ -1,5 +0,0 @@
-/Users/bruger/Documents/Github/Semester_project_trustworthy/esp32/target/debug/deps/libesp32_embedded-c7be5c301cb57ea5.rmeta: src/main.rs
-
-/Users/bruger/Documents/Github/Semester_project_trustworthy/esp32/target/debug/deps/esp32_embedded-c7be5c301cb57ea5.d: src/main.rs
-
-src/main.rs:
